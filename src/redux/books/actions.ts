5d727c4a6d25ae2eafc1b7f74ce88073555e116e
smilejin92 <smilejin92@gmail.com>
2020-11-20 18:16:20 +0900

    chore: Initialize redux folder and books module

---
+++ src/redux/books/actions.ts
@@ -0,0 +1,1 @@
+const prefix = 'get-my-books/books';
---
+++ src/redux/create.ts
@@ -0,0 +1,6 @@
+import { createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+export default function create() {
+  const sagaMiddleware = createSagaMiddleware();
+}
