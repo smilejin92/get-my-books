889bd12db1ef6186a06bfd653a4662c186f12d73
smilejin92 <smilejin92@gmail.com>
2020-11-22 23:16:28 +0900

    chore: Add pages components

---
+++ src/pages/Error.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Error: React.FC = () => <h1>에러다</h1>;
+
+export default Error;
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NotFound: React.FC = () => <h1>페이지를 찾을 수 없습니다.</h1>;
+
+export default NotFound;
