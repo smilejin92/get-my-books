315808139d3dba49c33bfec949f25aa3d331b7bf
smilejin92 <smilejin92@gmail.com>
2020-11-23 18:19:56 +0900

    feat: Set react-router on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import create from './redux/create';
+
+const store = create();
 
 function App() {
-  return <h1>Get My Books</h1>;
+  return (
+    <Provider store={store}>
+      <ErrorBoundary FallbackComponent={Error}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/results" component={Home} />
+            <Route exact path="/" component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </ErrorBoundary>
+    </Provider>
+  );
 }
 
 export default App;
